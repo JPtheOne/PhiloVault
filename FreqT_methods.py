451b43cffe996e9fe723bec5def98727f2d84f2c
JPtheOne <juanpimod@gmail.com>
2023-04-22 21:40:41 -0600

    QUERIES FINALLY WORK! Now is time to clean up and connect everything.

---
+++ FreqT_methods.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from gensim import corpora
+def FreqT_generation(docs):
+    # Create a dictionary based on the preprocessed documents
+    dictionary = corpora.Dictionary(docs)
+
+    # Convert the preprocessed documents to a bag-of-words representation
+    corpus = [dictionary.doc2bow(doc) for doc in docs]
+
+    # Calculate the term-frequency matrix
+    term_frequency_matrix = []
+
+    for doc in corpus:
+        row = [0] * len(dictionary)
+        for word_id, frequency in doc:
+            row[word_id] = frequency
+        term_frequency_matrix.append(row)
+
+    # Convert the term-frequency matrix to a Pandas DataFrame
+    term_frequency_df = pd.DataFrame(term_frequency_matrix, columns=[dictionary[id] for id in dictionary])
+    term_frequency_df_transposed = term_frequency_df.T
+    return term_frequency_df_transposed
+
+def apply_svd(tf_matrix, k=2):
+    # Convert the term-frequency DataFrame to a NumPy array
+    tf_matrix_np = tf_matrix.to_numpy()
+
+    # Perform SVD on the term-frequency matrix
+    U, Sigma, Vt = np.linalg.svd(tf_matrix_np, full_matrices=False)
+
+    # Truncate the U, Sigma, and Vt matrices based on the desired number of components (k)
+    U_k = U[:, :k]
+    Sigma_k = np.diag(Sigma[:k])
+    Vt_k = Vt[:k, :]
+
+    # Compute the reduced matrix by multiplying the truncated U, Sigma, and Vt matrices
+    reduced_matrix = U_k @ Sigma_k @ Vt_k
+
+    return reduced_matrix
---
+++ Machinery.py
@@ -0,0 +1,79 @@
+from FreqT_methods import *
+from Proximity_measures import*
+
+#-----------------------Testing Methods--------------------------#
+preprocessed_docs = [
+    ['survey',"god"],
+    ['survey',"god"], 
+    ['survey', 'user', 'computer', 'cat', 'cat', 'bird','bird'],
+    ['survey', 'user', 'computer', 'system', 'response', 'time']
+                    ]
+
+FreqT = (FreqT_generation(preprocessed_docs))
+print("Frequency Table: \n",FreqT)
+
+# Call your FreqT_generation method to obtain the term-frequency matrix
+term_frequency_matrix = FreqT_generation(preprocessed_docs)
+
+# Apply SVD to the term-frequency matrix and get the reduced matrix
+reduced_matrix = apply_svd(term_frequency_matrix, 3)
+
+# Convert the reduced matrix back to a Pandas DataFrame with the same format as the input term-frequency matrix
+printable_reduced_matrix_df = pd.DataFrame(reduced_matrix, index=term_frequency_matrix.index, columns=term_frequency_matrix.columns)
+
+print("Reduced Frequency Table: \n ", printable_reduced_matrix_df)
+
+mtx = reduced_matrix
+formula = "inner"
+#print(f"{formula} similarity: ", calculate_similarity(formula, mtx, 0,1))
+
+formula = "cosine"
+#print(f"{formula} disimilarity: ", calculate_dissimilarity(formula, mtx, 0,1))
+
+
+'''
+preprocessed_docs.append(query)
+
+New_FreqT = FreqT_generation(preprocessed_docs)
+print("NEW FREQT WITH Q IS: \n", New_FreqT)
+
+New_reducedMatrix = apply_svd(New_FreqT, 3)
+
+# Convert the New_reducedMatrix back to a Pandas DataFrame with the same format as the input term-frequency matrix
+printable_new_reduced_matrix_df = pd.DataFrame(New_reducedMatrix, index=New_FreqT.index, columns=New_FreqT.columns)
+print("Reduced Frequency Table (With Query): \n ", printable_new_reduced_matrix_df)
+
+
+New_Doc = New_reducedMatrix[:,-1]
+print("NEW DOC IS: ",New_Doc)
+'''
+
+query  = ["survey","god"]
+print("Query is: ", query)
+n = 2
+
+def find_bestDocs(query, n, preprocessed_docs):
+    similarity_scores = []
+
+    preprocessed_docs.append(query)
+
+    New_FreqT = FreqT_generation(preprocessed_docs)
+    print("NEW FREQT WITH Q IS: \n", New_FreqT)
+
+    New_reducedMatrix = apply_svd(New_FreqT, 3)
+    printable_new_reduced_matrix_df = pd.DataFrame(New_reducedMatrix, index=New_FreqT.index, columns=New_FreqT.columns)
+    print("Reduced Frequency Table (With Query): \n ", printable_new_reduced_matrix_df)
+
+    New_Doc = reduced_matrix[:,-1]
+    print("NEW DOC IS: ",New_Doc)
+
+
+    for doc in range(New_reducedMatrix.shape[1]-1):
+        similarity = calculate_similarity("Jac",New_reducedMatrix, doc, -1)
+        similarity_scores.append(similarity)
+    
+    return similarity_scores
+
+
+scores = (find_bestDocs(query, n, preprocessed_docs))
+print("Comparisons with New doc and other docs are: ", scores)
